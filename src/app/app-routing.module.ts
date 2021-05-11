2fc769d9d3f90352f2e432bb09149a437f9058ec
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-11 18:33:10 +0800

    added components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeedComponent } from './feed/feed.component';
+import { ProjectComponent } from './project/project.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'feed', component: FeedComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'feed/project/:id', component: ProjectComponent },
+  { path: '',   redirectTo: '/feed', pathMatch: 'full' }, // redirect to `feed`
+  { path: '**', component: PagenotfoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { FeedComponent } from './feed/feed.component';
+import { ProjectComponent } from './project/project.component';
+import { ProfileComponent } from './profile/profile.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    FeedComponent,
+    ProjectComponent,
+    ProfileComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
