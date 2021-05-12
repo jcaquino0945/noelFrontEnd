7f3bda48f012fddee0c70dc91ccbfb8b9e2d8e9b
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-12 21:38:41 +0800

    admin dashboard and profile

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,14 @@ import { FeedComponent } from './feed/feed.component';
 import { ProjectComponent } from './project/project.component';
 import { ProfileComponent } from './profile/profile.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { AdminProjectComponent } from './admin-project/admin-project.component';
 
 const routes: Routes = [
   { path: 'feed', component: FeedComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard/project/:id', component: AdminProjectComponent },
   { path: 'feed/project/:id', component: ProjectComponent },
   { path: '',   redirectTo: '/feed', pathMatch: 'full' }, // redirect to `feed`
   { path: '**', component: PagenotfoundComponent },
