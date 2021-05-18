c2c57101920c73f98766c7be5b1a7217297f8fbe
ced <bolinacedric23@gmail.com>
2021-05-18 23:16:47 +0800

    new project form x revised project.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,10 @@ import { ProfileComponent } from './profile/profile.component';
 import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatIconModule} from '@angular/material/icon';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent, DialogContentExampleDialog } from './dashboard/dashboard.component';
 import { AdminProjectComponent } from './admin-project/admin-project.component';
+
+import {MatDialogModule} from '@angular/material/dialog';
 
 @NgModule({
   declarations: [
@@ -22,13 +24,15 @@ import { AdminProjectComponent } from './admin-project/admin-project.component';
     ProfileComponent,
     PagenotfoundComponent,
     DashboardComponent,
-    AdminProjectComponent
+    AdminProjectComponent,
+    DialogContentExampleDialog
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    MatIconModule,
+    MatDialogModule
   ],
   providers: [],
   bootstrap: [AppComponent]
