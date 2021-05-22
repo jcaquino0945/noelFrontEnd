42045015eebce4c86b98289e785a9496ba84b989
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-22 18:49:50 +0800

    profile display

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
+  currentUser;
 
-  constructor() { }
+  constructor(
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    console.log('profile init')
+    this.currentUser = this.authService.getUserDetails();
+    console.log(this.currentUser)
+
   }
 
 }
