761bc30724e411535a8e6a98a394548a6b9bd873
ced <bolinacedric23@gmail.com>
2021-05-23 00:07:26 +0800

    users tab  updated

--- src/app/admin-project/admin-project.component.ts
+++ src/app/admin-project/admin-project.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../services/project.service';
 import { Project } from '../models/project';
+import { Params, ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-project',
@@ -9,12 +11,33 @@ import { Project } from '../models/project';
 })
 export class AdminProjectComponent implements OnInit {
   projects$: Project[];
+  project: Project;
+  projectIds: string[];
 
   constructor(
-    private projectService: ProjectService
-  ) { }
+    private projectService: ProjectService,
+    private route: ActivatedRoute,
+    ) { }
 
   ngOnInit(): void {
+    this.projectService.getProjectIds().subscribe(
+      (projectIds) => (this.projectIds = projectIds)
+    )
+    this.route.params
+      .pipe(
+        switchMap((params: Params) => {
+          return this.projectService.getProject(params['id']);
+        })
+      )
+      .subscribe(
+        (project) => {
+          this.project = project;
+        }
+      );
+
+      this.projectService.getProjects().subscribe(
+        (projects$) => (this.projects$ = projects$)
+      );
   }
 
 }
