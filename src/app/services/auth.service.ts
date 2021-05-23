564a4a26521f80e0640525432b1a95937d3b66e6
jcaquino0945 <jcaquino0945@gmail.com>
2021-05-23 15:01:19 +0800

    added user verification

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,12 +1,28 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+  HttpParams,
+  HttpRequest,
+} from '@angular/common/http';
+import { catchError, map } from 'rxjs/operators';
+import { ProcessHTTPMsgService } from './process-httpmsg.service';
+
+const apiUrl = 'http://localhost:3000/users'; //api url from backend service
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor(private http: HttpClient) {}
+  headers = new HttpHeaders({
+    'Content-Type': 'application/json',
+    //'Authorization': 'bearer ' + sessionStorage.getItem('token')
+ });
+  constructor(private http: HttpClient,
+    private processHTTPMsgService: ProcessHTTPMsgService
+    ) {}
   public getUserDetails() {
     let user = JSON.parse(sessionStorage.getItem('user'));
     return user;
@@ -24,7 +40,6 @@ export class AuthService {
   public setUserInfo(token,user) {
     sessionStorage.setItem('token',token);
     sessionStorage.setItem('user',JSON.stringify(user));
-
   }
 
   public validate(username, password) {
@@ -34,6 +49,43 @@ export class AuthService {
         password: password,
       })
       .toPromise();
+  }
+  public register(username,password,name,contactNumber,birthday,email):Observable<any> {
+    console.log( username)
+    console.log( password)
+    console.log( name)
+    console.log( contactNumber)
+    console.log( birthday)
+    console.log( email)
+    
+    let data = JSON.stringify({
+      "username": username,
+      "password": password,
+      "name": name,
+      "contactNumber": contactNumber,
+      "birthday": birthday,
+      "email": email,
+      "admin": false,
+      "verified": false
+    });
+    console.log(data)
+
+
+
+    const params = new HttpParams();
+    let myHeaders = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*'
+      //'Authorization': 'bearer ' + sessionStorage.getItem('token')
+   });
+    const options = {
+      params,
+      reportProgress: true,
+      headers: myHeaders,
+    };
+    const req = new HttpRequest('POST', apiUrl + '/signup',data, options);
+    return this.http.request(req);
+
   }
 
   public clear() {
